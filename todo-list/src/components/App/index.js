20f17f8a39e66283f116486d8b4920e459691eea
arsalan ghofrani <Arsalan@arsalans-MacBook-Pro.local>
2020-05-07 17:58:13 +0430

    refactor feature2 using hooks

--- todo-list/src/components/App/index.js
+++ todo-list/src/components/App/index.js
@@ -1,68 +1,47 @@
-import React from 'react';
+import React,{useState} from 'react';
 
-class App extends React.Component {
-  constructor(props) {
-    super(props);
+const list=[{
+    task:"asasa",
+    id:'223'
+},{
+    task:"fbubf",
+    id:'121111'
+},{
+    task:"quwbdq",
+    id:"1212132"
+}];
 
-    this.state={
-      list:[
-        {
-          task:"item1",
-          id:123
-        },
-        {
-          task:"item2",
-          id:12
-        },
-        {
-          task:"item3",
-          id:1
-        }
-      ],
-      inputValue:"a"
-    };
-  }
 
-  inputChangeHandler = (e) => {
-    const value= e.target.value  
-    this.setState((prevState)=> ({
-      ...prevState,inputValue : value
-    }))
-  }
-  inputSubmitHandler = (event) =>{
-    event.preventDefault();
-    this.setState((prevState) => ({
-      ...prevState,
-      list: prevState.list.concat({
-        task: prevState.inputValue,
-      id:Math.random()
-    }),
-      inputValue: '',
-    }))
-  }
-  
-  render () {
-    return(
-      <div>
-        <ul>
-      {this.state.list.map((item) => {
-        return (
-        <li key={item.key}>{item.task}</li>
-        )
-      })}
+
+
+const App = (prop) => {
+    const [todoItems,setTodoItems] = useState(list)
+    const [inputValue,setInputValue] = useState("")
+    const inputChangeHandler = (e) => {
+        setInputValue(e.target.value);
+    }
+    const submitHandler = (e) => {
+        e.preventDefault();
+        setTodoItems((prevTodoItems) => prevTodoItems.concat({ task:inputValue, id:Math.random()}))
+        setInputValue("");
+    }
+    return (
+        <>
+            
+            <ul>
+            {todoItems.map((item) => {
+                return (
+                        <li key={item.id}>{item.task}</li>
+                       )}
+                       )
+            }
         </ul>
-         <form onSubmit={this.inputSubmitHandler}>
-           <input type="text"
-           onChange={(e) => this.inputChangeHandler(e)}
-           value={this.state.inputValue}
-           />
-           <input type="submit" />
-        </form>
-      </div>
-    )
-  }
+        <form onSubmit={submitHandler}>
+            <input type="text" onChange={inputChangeHandler} value={inputValue} />
+            <input type="submit"/>
+        </form>  
+        </>     
+    );
 }
-
-
 
 export default App;
