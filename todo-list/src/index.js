7974193e693944bb060bbf3c13baaa5208afda4f
arsalan ghofrani <Arsalan@arsalans-MacBook-Pro.local>
2020-05-07 17:45:08 +0430

    feature2 is refactored using hooks

--- todo-list/src/index.js
+++ todo-list/src/index.js
@@ -1,6 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './components/App';
+import App from './components/App/index';
+
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
 
 
-ReactDOM.render(<App/>, document.querySelector("#root"));
